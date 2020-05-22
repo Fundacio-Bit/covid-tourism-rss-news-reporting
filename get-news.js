375b5ac4f939e51ce250674308e939cca6cdb84c
omoya_fbit <omoya@fundaciobit.org>
2020-05-22 12:47:39 +0200

    Added filtyer by category

--- get-news.js
+++ get-news.js
@@ -1,6 +1,34 @@
-// var mongo = require("mongodb");
 var MongoClient = require("mongodb").MongoClient;
-// var bodyParser = require("body-parser");
+var d3 = require("d3");
+var categoriesDict = require("./categories_dictionary.js");
+
+let brands = [
+  "mallorca",
+  "menorca",
+  "ibiza",
+  "formentera",
+  "baleares",
+  "españa",
+];
+
+let getNewsPerBrandMarketCategory = (news) => {
+  let newsByBrandByCountry = d3
+    .nest()
+    .key(function (doc) {
+      return doc.brand;
+    })
+    .key(function (doc) {
+      return doc.country;
+    })
+    .key(function (doc) {
+      return doc.category;
+    })
+    .rollup(function (brandGroup) {
+      return brandGroup.length;
+    })
+    .entries(news);
+  console.log(JSON.stringify(newsByBrandByCountry));
+};
 
 // Initialize connection once
 MongoClient.connect(
@@ -13,18 +41,20 @@ MongoClient.connect(
     let db = client.db("rss_covid_tourism_db");
     let mongoQuery = {};
     let mongoProjection = {
-      _id: 1,
-      published: 1,
-      extraction_date: 1,
-      brand: 1,
-      title: 1,
-      topics: 1,
-      link: 1,
-      summary: 1,
-      description: 1,
-      section: 1,
-      source_id: 1,
-      source_name: 1,
+      projection: {
+        _id: 0,
+        published: 0,
+        extraction_date: 0,
+        topics: 0,
+        link: 0,
+        summary: 0,
+        description: 0,
+        section: 0,
+        sourec_name: 0,
+        selected: 0,
+        content_value: 0,
+        tags: 0,
+      },
     };
     let collection = db.collection("news");
 
@@ -36,7 +66,44 @@ MongoClient.connect(
           console.log(err);
           res.status(500).send(err);
         } else {
-          console.log(docs);
+          // get country from source_id
+          let docsWithCountry = docs.map((doc) => {
+            if (doc.source_id.includes("AIR")) {
+              doc.country = "ES";
+            } else {
+              doc.country = doc.source_id.split("_")[1];
+            }
+            return doc;
+          });
+
+          let docsWithCountryAndCategory = docsWithCountry.map((doc) => {
+            // assign a category for each document after cheking if any term belonging to that category appears in any
+            // content field. Category values are: covid, tourism, both and none.
+
+            Object.keys(categoriesDict).forEach((key) => {
+              for (i = 0; i < categoriesDict[key].length; i++) {
+                if (doc.title.toLowerCase().includes(categoriesDict[key][i])) {
+                  if ("category" in doc && doc.category !== "none") {
+                    doc.category = "both";
+                    console.log("both");
+                    break;
+                  } else {
+                    doc.category = key;
+                    console.log(key);
+                    break;
+                  }
+                } else {
+                  if ("category" in doc) {
+                    continue;
+                  } else {
+                    doc.category = "none";
+                  }
+                }
+              }
+            });
+            return doc;
+          });
+          getNewsPerBrandMarketCategory(docsWithCountryAndCategory);
         }
       });
   }
